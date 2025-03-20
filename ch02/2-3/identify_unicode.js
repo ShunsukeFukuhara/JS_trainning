cdd6a1160579d1ea44e0b478fcc145ed0471509a
RFG\r00528148 <Shunsuke.Fukuhara@jp.ricoh.com>
2025-03-20 18:39:16 +0900

    2-3修正

--- ch02/2-3/identify_unicode.js
+++ ch02/2-3/identify_unicode.js
@@ -1,26 +1,25 @@
 import fs from "fs";
-import path from "path";
 
-// 実際のファイルパスを指定
-const filePath = "éxample.txt";
+// NFCのファイルパス
+const nfcFilePath = "éxample.txt";
+
+// NFDのファイルパス
+const nfdE = "e\u0301";
+const nfdFilePath = `${nfdE}xample.txt`;
 
 // ファイルが存在するか確認
-fs.access(filePath, fs.constants.F_OK, (err) => {
+fs.access(nfcFilePath, fs.constants.F_OK, (err) => {
   if (err) {
-    console.log("ファイルが存在しません:", err);
     return;
   }
 
-  // ファイル名を取得
-  const fileName = path.basename(filePath);
+  console.log("NFCファイルが存在します");
+});
 
-  // 正規化してNFCに変換
-  const fileNameNFC = fileName.normalize("NFC");
+fs.access(nfdFilePath, fs.constants.F_OK, (err) => {
+  if (err) {
+    return;
+  }
 
-  // ファイル名とその正規化結果を比較する
-  console.log("元のファイル名:", fileName);
-  console.log("NFC形式での正規化:", fileNameNFC);
-
-  // 同一かどうかを比較
-  console.log("NFCと同一か?", fileName === fileNameNFC);
+  console.log("NFDファイルが存在します");
 });
