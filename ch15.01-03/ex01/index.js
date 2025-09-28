19f3d2eb2ed79c85ebb354a6bd0ae69e06979013
RFG\r00528148 <Shunsuke.Fukuhara@jp.ricoh.com>
2025-09-28 23:35:38 +0900

    ch15/ex01

--- ch15.01-03/ex01/index.js
+++ ch15.01-03/ex01/index.js
@@ -4,6 +4,8 @@ const input = document.querySelector("#new-todo");
 
 form.addEventListener("submit", (e) => {
   // TODO: ここで form のイベントのキャンセルを実施しなさい (なぜでしょう？)
+  // 理由: フォームのデフォルトの送信動作を防ぎ、ページのリロードを防ぐため
+  e.preventDefault();
 
   // 両端からホワイトスペースを取り除いた文字列を取得する
   if (input.value.trim() === "") {
@@ -22,10 +24,28 @@ form.addEventListener("submit", (e) => {
 
   const toggle = document.createElement("input");
   // TODO: toggle が変化 (change) した際に label.style.textDecorationLine を変更しなさい
+  toggle.type = "checkbox";
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      label.style.textDecorationLine = "line-through";
+    } else {
+      label.style.textDecorationLine = "none";
+    }
+  });
 
   const destroy = document.createElement("button");
   // TODO: destroy がクリック (click) された場合に elem を削除しなさい
+  destroy.textContent = "❌";
+  destroy.addEventListener("click", () => {
+    elem.remove();
+  });
+
+  elem.appendChild(toggle);
+  elem.appendChild(label);
+  elem.appendChild(destroy);
 
   // TODO: elem 内に toggle, label, destroy を追加しなさい
+  elem.append(toggle, label, destroy);
+
   list.prepend(elem);
 });
