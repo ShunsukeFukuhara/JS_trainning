5d5af47b5c7b8111cb95fb70c321091774c3aeea
RFG\r00528148 <Shunsuke.Fukuhara@jp.ricoh.com>
2025-11-16 12:38:28 +0900

    ex07

--- ch15.04-10/ex06/index.js
+++ ch15.04-10/ex06/index.js
@@ -21,6 +21,52 @@ class TodoApp extends HTMLElement {
 
     this.form = this.shadowRoot.querySelector("#new-todo-form");
     // TODO: 残りを実装
+    this.input = this.shadowRoot.querySelector("#new-todo");
+
+    // フォームの送信イベント
+    this.form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      if (this.input.value.trim() === "") {
+        return;
+      }
+
+      this.addTodo(this.input.value);
+      this.input.value = "";
+    });
+  }
+
+  addTodo(text) {
+    const list = this.shadowRoot.querySelector("#todo-list");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    // チェックボックスの変更イベント
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        span.classList.add("completed");
+      } else {
+        span.classList.remove("completed");
+      }
+    });
+
+    const listItem = list.appendChild(document.createElement("li"));
+    listItem.appendChild(checkbox);
+    listItem.appendChild(span);
+    // 追加したアイテムを先頭に表示
+    list.insertBefore(listItem, list.firstChild);
+
+    // アイテムから先頭の・を削除
+    listItem.style.listStyle = "none";
+
+    // submit時に、完了したアイテムをリストの最後に移動
+    this.form.addEventListener("submit", () => {
+      if (checkbox.checked) {
+        list.appendChild(listItem);
+      }
+    });
   }
 }
 
