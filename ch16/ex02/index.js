efdf49274ab4ed60f19339ec9c903f9dc819263d
RFG\r00528148 <Shunsuke.Fukuhara@jp.ricoh.com>
2026-01-01 00:51:49 +0900

    ex02

--- ch16/ex02/index.js
+++ ch16/ex02/index.js
@@ -31,3 +31,44 @@ async function startChild() {
 }
 
 // TODO: ここに処理を書く
+const shutdownSignals = [
+  // Ctrl+C
+  { signal: "SIGINT", posixCode: 130, windowsCode: 3221225786 },
+  // Break
+  { signal: "SIGBREAK", posixCode: 21, windowsCode: 3221225786 },
+];
+
+shutdownSignals
+  .map((s) => s.signal)
+  .forEach((signal) => {
+    process.on(signal, () => {
+      console.log(`${signal}を検知しました。子プロセスに伝達します。`);
+      child?.kill(signal);
+    });
+  });
+
+(async () => {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const [code, signal] = await startChild();
+    console.log(`子プロセスが終了しました。code: ${code}, signal: ${signal}`);
+
+    const mathcedSignal = shutdownSignals.find(
+      (s) =>
+        s.signal === signal ||
+        (process.platform === "win32" ? s.windowsCode : s.posixCode) === code
+    );
+
+    if (mathcedSignal) {
+      console.log(
+        `親プロセスも${mathcedSignal.signal}を受け取ったので終了します。`
+      );
+      process.exit(mathcedSignal.posixCode);
+    }
+
+    if (code !== 0) {
+      console.log("子プロセスが異常終了しました。再起動します。");
+      continue;
+    }
+  }
+})();
